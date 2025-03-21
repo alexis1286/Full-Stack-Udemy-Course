70f819f33bcc47c6f33d3092daa14b9957d84b0c
Alexis <alovitzschool@gmail.com>
2025-03-21 00:28:35 -0400

    learning node and npm

---
+++ Section 23/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// import generateName from "sillyname";
+// const sillyName = generateName();
+
+// console.log(`my name is ${sillyName}.`);
+
+
+import gerenateSuperhero from "superheros";
+
+const name = gerenateSuperhero();
+
+console.log(`I am ${name}!`);
