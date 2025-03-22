5e697645c45c29b9e0845a8566bb3ec2d9e078de
Alexis <alovitzschool@gmail.com>
2025-03-21 21:21:48 -0400

    qr code maker in js

--- Section 23/2.4+QR+Code+Project/2.4 QR Code Project/index.js
+++ Section 23/2.4+QR+Code+Project/2.4 QR Code Project/index.js
@@ -3,3 +3,45 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+
+import inquirer from "inquirer";
+import fs from 'fs';
+import qr from 'qr-image';
+var ans;
+inquirer
+	.prompt([
+		{
+			name: "url",
+			message: "What is the URL you want to be made into a QR Code?",
+		},
+	])
+	.then((answers) => {
+        ans = answers.url;
+   
+        fs.writeFile("userInput.txt", ans, (err) => {
+			if (err) {
+				console.error(err);
+			} else {
+                console.log("File written successfully");
+				// file written successfully
+			}
+		});
+
+
+        //create the qr code 
+		const qr_png = qr.image(ans, { type: "png" });
+        //save the qr image
+		qr_svg.pipe(fs.createWriteStream("UsersURL.png"));
+
+	})
+	.catch((error) => {
+		if (error.isTtyError) {
+			// Prompt couldn't be rendered in the current environment
+		} else {
+			// Something else went wrong
+		}
+	});
+
+
+		
