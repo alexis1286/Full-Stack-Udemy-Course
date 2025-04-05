31454a8651b41ad76dff34ad8c30be488402c6d6
Alexis <alovitzschool@gmail.com>
2025-04-04 20:38:56 -0400

    learning more middleware

--- Section 24/3.4+Middleware/3.4 Middleware/index3.js
+++ Section 24/3.4+Middleware/3.4 Middleware/index3.js
@@ -1,7 +1,16 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 const port = 3000;
+
+function logger(req,res, next){
+  //the first console log grabs the method used in the request
+    console.log("Response: ", req.method);
+    //the second line will grab the url the request is coming from 
+    console.log("URL: ",  req.url);
+    //you need this next method bc otherwise itll hang, it never reaches the next line
+    next();
+}
 
 app.use(logger);
 
--- Section 24/3.4+Middleware/3.4 Middleware/index4.js
+++ Section 24/3.4+Middleware/3.4 Middleware/index4.js
@@ -1,8 +1,42 @@
 import express from "express";
+import bodyParser from "body-parser";
+//lines of code to get the location of index.html project
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var name = "";
+
+//middleware to parse thru the input so we can then display it
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//have to make a function to get the body input and concant it to a var
+//making ur own middleware basically
+function bandname(req, res, next) {
+	name = req.body["street"] + req.body["pet"];
+	next();
+}
+
+//have to run the function
+app.use(bandname);
+
+//displays the index.html onto our site
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+	//output the name that we inputted into our generator using our function and the name var it created
+	//couldve also just written this here:
+
+	//name = req.body["street"] + req.body["pet"];
+
+	//and not had the function at all, it wouldve worked also
+	res.send(`<h1>Your Bandname is:<br>${name}🎸🤘🔥<h1>`);
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
